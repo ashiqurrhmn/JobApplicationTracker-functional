be1d7147f24bc74512c7e7d4b18ee2f58465513d
ashiqurrhmn <arakash115@gmail.com>
2026-02-22 19:53:36 +0600

    detect duplicate info

--- srcipt/script.js
+++ srcipt/script.js
@@ -13,26 +13,48 @@ const allBtn = document.getElementById("all-btn");
 const interviewBtn = document.getElementById("interview-btn");
 const rejectedBtn = document.getElementById("rejected-btn");
 
-function count(){
-    total.innerText = allCards.children.length;
-    availableJobs.innerText = `${allCards.children.length} jobs`;
-    interview.innerText = interviewList.length;
-    rejected.innerText = rejectedList.length;
+function count() {
+  total.innerText = allCards.children.length;
+  availableJobs.innerText = `${allCards.children.length} jobs`;
+  interview.innerText = interviewList.length;
+  rejected.innerText = rejectedList.length;
 }
 count();
 
-function toggleStyle(id){
-    allBtn.classList.remove('bg-blue-500', 'text-white');
-    interviewBtn.classList.remove('bg-blue-500', 'text-white');
-    rejectedBtn.classList.remove('bg-blue-500', 'text-white');
+function toggleStyle(id) {
+  allBtn.classList.remove("bg-blue-500", "text-white");
+  interviewBtn.classList.remove("bg-blue-500", "text-white");
+  rejectedBtn.classList.remove("bg-blue-500", "text-white");
 
-    allBtn.classList.add('bg-white', 'text-[#64748B]');
-    interviewBtn.classList.add('bg-white', 'text-[#64748B]');
-    rejectedBtn.classList.add('bg-white', 'text-[#64748B]');
+  allBtn.classList.add("bg-white", "text-[#64748B]");
+  interviewBtn.classList.add("bg-white", "text-[#64748B]");
+  rejectedBtn.classList.add("bg-white", "text-[#64748B]");
 
-    const selectedBtn = document.getElementById(id);
+  const selectedBtn = document.getElementById(id);
 
-    selectedBtn.classList.remove('bg-white', 'text-[#64748B]');
-    selectedBtn.classList.add('bg-blue-500', 'text-white');
-    
+  selectedBtn.classList.remove("bg-white", "text-[#64748B]");
+  selectedBtn.classList.add("bg-blue-500", "text-white");
 }
+
+mainContainer.addEventListener("click", function (event) {
+  const parent = event.target.parentNode.parentNode;
+  const jobName = parent.querySelector(".job-name").innerText;
+  const position = parent.querySelector(".position").innerText;
+  const time = parent.querySelector(".time").innerText;
+  const status = parent.querySelector(".status-name").innerText;
+  const description = parent.querySelector(".description").innerText;
+  
+  const cardInfo = {
+    jobName,
+    position,
+    time,
+    status,
+    description,
+  }
+  const existingCard = interviewList.find(item=> item.jobName == cardInfo.jobName)
+  if(!existingCard) {
+    interviewList.push(cardInfo);
+  }
+  console.log(interviewList);
+  count();
+});
